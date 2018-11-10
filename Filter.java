d2d6a964b5b518f88d0df55305928cad3812dca0
o.vdovichenko <o.vdovichenko@student.csn.khai.edu>
2018-11-10 19:33:40 +0200

    Comments

--- Filter.java
+++ Filter.java
@@ -1,21 +1,38 @@
+/* Lab 3d - Simple JAVA program. Support class
+* There is Nothing to return
+**
+@author Vdovichenko A.A.
+* @version Alpha 2.0
+* @since 2018-10-28
+*/
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Filter {
-	public static int D;
-	public static long E;
-	public static double P;
-	public static double m;
-	public static double d;
+	/**
+	* Class for analyzing data stream 
+	* There is Nothing to return
+	**
+	@param args receives a string -- path to input 
+	and output files 
+	* @return maybe in later versions
+	*/
+	public static int D;	//Dynamic range
+	public static long E;	//Energy
+	public static double P; //Power
+	public static double m; //Middle signals count
+	public static double d; //Disperse signals count
 	
 	public static int Counter;
 	public static int Sum;
-	public static int Max;
-	public static int Min;
-	public static int QSum;
-	
+	public static int Max;	//Value of signal
+	public static int Min;	//Value of signal
+	public static int QSum;	//Sum of squares
+	public static List<Integer> dis;
+							//dynamic array
 	public Filter() {
-		D = 0;
+		D = 0;				
 		E = 0;
 		P = 0;
 		m = 0;
@@ -26,40 +43,46 @@ public class Filter {
 	  Max = 0;
 	  Min = 255;
 	 QSum = 0;
+	 dis = new ArrayList();
 	}
 	public static int Bite(int i, byte j) {
-		int[] slice = new int[2] ;
-		slice[0] = (byte) (i & 0xFF);
-		slice[1] = (byte) ((i >> 8) & 0xFF);
-		return slice[j];
+					//Converting integer to byte Method
+		int[] slice = new int[2] ; //navigation signals
+		slice[0] = (byte) (i & 0xFF);	//first part
+		slice[1] = (byte) ((i >> 8) & 0xFF); //second
+		return slice[j];	
 	}
 	public static void Disperser(int income) {
-		List<Integer> dis = new ArrayList();
-		double disperse = 0;
+			//method for asynchronic disperse counting
+		double disperse = 0;	
 		if(income == -1) {
 			for(int i = 0; i < dis.size(); i++)
 			{
-				disperse += ((double)dis.get(i) - m)*((double)dis.get(i) - m);
+				disperse += ((double)dis.get(i) - m)
+						*((double)dis.get(i) - m);
+				//disperse counting
 			}
 			d = disperse / (double)Counter;
 		}
-		dis.add(income);
+		dis.add(income);	//memorizing data
 		
 	}
-	static Filter frnd = new Filter();
+	static Filter frnd = new Filter();	//filter object
 	public static void Analizer(int income) {
-		
+				//method for synchronic data analizing
+		if(income != -1) {
 		Counter ++; 
-		Sum =+ income;
-		QSum =+ income * income;
-		if(income > Max) Max = income;
-		if(income < Min) Min = income;
-		frnd.Disperser(income);
+		Sum =+ income;					//Global sum
+		QSum =+ income * income;		//QSum
+		if(income > Max) Max = income;	//Max filter
+		if(income < Min) Min = income;	//Min filter
+		}
+		frnd.Disperser(income);		//disperse call
 		if(income == -1) {
-			D = Max - Min;
-			E = QSum;
-			P =(double) QSum / (double)Counter;
-			m = (double)Sum / (double)Counter;
+			D = Max - Min;			//format saving
+			E = QSum;				//format saving
+			P =(double)QSum / (double)Counter;
+			m =(double)Sum / (double)Counter;
 			
 		}
 		
--- Importer.java
+++ Importer.java
@@ -1,5 +1,23 @@
-	import java.io.*;
+
+/* Lab 3d - Simple JAVA program. Main class
+* There is Nothing to return
+**
+@author Vdovichenko A.A.
+* @version Alpha 2.0
+* @since 2018-10-28
+*/
+
+	
+import java.io.*;
 	class Importer {
+		/**
+		* Class for re-writing file with synchro-analyzing 
+		* There is Nothing to return
+		**
+		@param args receives a string -- path to input 
+		and output files 
+		* @return maybe in later versions
+		*/
 	public static void main (String args[ ]) throws IOException
 	{
 	 int i;
@@ -8,54 +26,59 @@
 	 FileOutputStream fout;
 	 try {
 		 try {
-			 fin = new FileInputStream(args[0]);
+			 fin = new FileInputStream(args[0]);	//Input open
 		 	} 
 		 catch (FileNotFoundException e) {
-			 System.out.println ("File not found");
+			 System.out.println ("File not found");	//Warning
 			 return;
 		 	}
 		 try {
-			 fout = new FileOutputStream(args[1]);
+			 fout = new FileOutputStream(args[1]);	//Output open
 		 	}
 		 catch (FileNotFoundException e) {
-			 System.out.println ("File not found»");
+			 System.out.println ("File not found»"); //Warning
 			 return;
 		 	}
 	 	}
 	 catch (ArrayIndexOutOfBoundsException e) {
 		 System.out.println ("Using: CopyFile default");
-		 return;
+		 return;								//Warning message
 	 	}
-	 Filter Part = new Filter();
+	 Filter Part = new Filter();		//Analyzing class object
 	 try {
 		 
 		 do {
-			 i = fin.read();
-			
+			 i = fin.read();			//reading a 2 byte slice
 			 if(i !=- 1)
 				 {
-				 	
 				 	fout.write( Part.Bite(i, (byte) 0));
+				 						//writing a 1 byte slice(first)
 				 	Part.Analizer(Part.Bite(i, (byte) 0));
+				 						//analyzing a 1 byte slice(first)
 				 	if(Part.Bite(i, (byte) 1) > 0) {
+				 		
 				 		fout.write( Part.Bite(i, (byte) 1));
+				 						//writing a 1 byte slice(second)
 				 		Part.Analizer(Part.Bite(i, (byte) 1));
+				 						//analyzing a 1 byte slice(second)
 				 	}
 				 }
 		 } while (i != -1);
-		 Part.Analizer(Part.Bite(-1, (byte) 1));
+		Part.Analizer(Part.Bite(-1, (byte) 1)); //launching a count
 	 }
 	 catch (IOException e) {
-		 System.out.println ("File Error");
-	 	}
-	 System.out.println(Part.D);
-	 System.out.println(Part.E);
-	 System.out.println(Part.P);
-	 System.out.println(Part.m);
-	 System.out.println(Part.d);
-	 fin.close();
-	 fout.close();
+		 System.out.println ("File Error");		//Warning
 	 }
-	}
+	 finally{
+		 System.out.format(" D = %d%n", Part.D);	//Dynamic range
+		 System.out.format(" E = %d%n", Part.E);	//Energy
+		 System.out.format(" P = %.4f%n", Part.P);	//Power
+		 System.out.format(" m = %.4f%n", Part.m);	//Middle signals count
+		 System.out.format(" d = %.4f%n", Part.d);	//Disperse signals count
+		 fin.close();	//Input close
+		 fout.close();	//Output close
+	 	}
+	 }
+}
 
 
